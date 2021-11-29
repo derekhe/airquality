c871ca5abf21673c3f02d63fedd0883e3dce611a
derekhe <derekhe@april1985.com>
2021-11-29 21:04:53 +0800

    Add color and text

--- main.py
+++ main.py
@@ -29,10 +29,21 @@ while True:
         pm1_0 = (buff[8] << 8) + buff[9]
         pm2_5 = (buff[10] << 8) + buff[11]
         pm10 = (buff[12] << 8) + buff[13]
-
-        CWriter.set_textpos(ssd, 0, 0)
-        wri = CWriter(ssd, arial35, WHITE, BLACK)  # Report on fast mode. Or use verbose=False
+        pmmax = max(pm2_5, pm10)
+        color = BLACK
+        quality = ""
+        if pmmax < 50:
+            color = GREEN
+            quality = "Good"
+        elif pmmax < 100:
+            color = YELLOW
+            quality = "Not good"
+        else:
+            color = RED
+            quality = "Bad"
+        ssd.fill(color)
+        wri = CWriter(ssd, arial35, BLACK, color)  # Report on fast mode. Or use verbose=False
         wri.set_clip(True, True, False)
-        lbltim = Label(wri, 0, 0, 35)
-        lbltim.value('PM2.5:\n {:02d}\nPM10:\n {:02d}'.format(pm2_5, pm10))
-        refresh(ssd)
+        lbltim = Label(wri, 0, 0, 10)
+        lbltim.value('PM2.5:\n {:02d}\nPM10:\n {:02d}\n{}'.format(pm2_5, pm10, quality))
+        refresh(ssd, False)
